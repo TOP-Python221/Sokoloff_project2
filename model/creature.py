514d27d5ca4b9789d12dd90539695715568a0025
Gennadiy <renardes@gmail.com>
2022-12-02 11:39:38 +0500

    исправления

--- model/creature.py
+++ model/creature.py
@@ -1,7 +1,11 @@
+# ИСПРАВИТЬ: внимательнее – здесь вы импортировали модуль и именем объекта модуля аннотировали параметры в методах, когда вам нужен объект класса из этого модуля
 import datetime as dt
+
 __all__ = [
     'Creature'
 ]
+
+
 class Body:
     def __init__(self, health: int,
                  stamina: int,
@@ -12,10 +16,13 @@ class Body:
         self.hunger = hunger
         self.thirst = thirst
 
+
 class Mind:
-    def __init__(self, patterns: dict,
+    def __init__(self,
+                 patterns: dict,
                  joy: int,
                  anger: int):
+        # ИСПРАВИТЬ: это должно быть атрибутом класса, не экземпляра
         self.patterns = patterns
         self.joy = joy
         self.anger = anger
@@ -25,72 +32,38 @@ class Mind:
         pass
 
 
-
 class Creature:
     def __init__(self, name: str,
                  birthdate: dt,
                  body: Body,
                  mind: Mind):
         self.name = name
-        self.brithdate = birthdate
+        self.birthdate = birthdate
         self.body = body
         self.mind = mind
 
-
-        #tired()
-       # sleep()
-
+        # tired()
+        # sleep()
 
 
 
 class Cat(Creature):
     pass
 
+
 class Dog(Creature):
     pass
 
+
 class Fox(Creature):
     pass
+
 
 class Bear(Creature):
     pass
 
 
 
-
-
 class CreatureActions(Creature):
     pass
 
-
-
-
-
-class BodyState:
-    def __init__(self, timestamp: dt,
-                 health:int,
-                 stamina:int,
-                 hunger: int,
-                 thirst: int):
-        self.timestamp = timestamp
-        self.stamina = stamina
-        self.health = health
-        self.hunger = hunger
-        self.thirst = thirst
-
-class MindState:
-    def __init__(self, timestamp: dt,
-                 joy: int,
-                 anger: int,
-                 pattern):
-        self.timestamp = timestamp
-        self.joy = joy
-        self.anger = anger
-        self.pattern = pattern
-
-
-class StatesManger:
-    def __init__(self, body_history: list[BodyState],
-                 mind_last: MindState):
-        self.body_history = body_history
-        self.mind_last = mind_last
--- model/states.py
+++ model/states.py
@@ -1,19 +1,44 @@
+from datetime import datetime as dt
+
 __all__ = [
     'StatesCalculator'
 ]
 
 
 class BodyState:
-    pass
+    def __init__(self,
+                 timestamp: dt,
+                 health:int,
+                 stamina:int,
+                 hunger: int,
+                 thirst: int):
+        self.timestamp = timestamp
+        self.stamina = stamina
+        self.health = health
+        self.hunger = hunger
+        self.thirst = thirst
 
 
 class MindState:
-    pass
+    def __init__(self,
+                 timestamp: dt,
+                 joy: int,
+                 anger: int,
+                 pattern):
+        self.timestamp = timestamp
+        self.joy = joy
+        self.anger = anger
+        self.pattern = pattern
 
 
 class StatesManager:
-    pass
+    def __init__(self,
+                 body_history: list[BodyState],
+                 mind_last: MindState):
+        self.body_history = body_history
+        self.mind_last = mind_last
 
 
 class StatesCalculator:
     pass
+
