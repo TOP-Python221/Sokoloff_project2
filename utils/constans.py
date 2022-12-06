846f98109f6b8ddebacfb0fa5e8153fc2930aa7f
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2022-12-06 23:24:33 +0800

    Добавлен файл constans.py &gunctions.py

---
+++ utils/constans.py
@@ -0,0 +1,42 @@
+from enum import Enum
+from pathlib import Path
+from collections.abc import Sequence, Callable
+from typing import Annotated, TypedDict
+
+
+class Kind(Enum):
+    CAT = 'cat'
+    DOG = 'dog'
+    FOX = 'fox'
+    BEAR = 'bear'
+    SNAKE = 'snake'
+    LIZARD = 'lizard'
+    TURTLE = 'turtle'
+    # ...
+
+
+class Matureness(str, Enum):
+    EGG = 'egg'
+    CUB = 'cub'
+    YOUNG = 'young'
+    ADULT = 'adult'
+    ELDER = 'elder'
+
+    @classmethod
+    def mammals(cls):
+        res = list(cls)
+        res.remove(cls.EGG)
+        return res
+
+
+pathlike = str | Path
+
+KindActions = dict[Kind, Sequence[Callable]]
+ParamRanges = Sequence[Annotated[Sequence[int], 2]]
+RangesDict = TypedDict('RangesDict', {
+    'health': ParamRanges,
+    'stamina': ParamRanges,
+    'hunger': ParamRanges,
+    'thirst': ParamRanges
+})
+MatureDays = Annotated[Sequence[int], 3] | Annotated[Sequence[int], 4]
