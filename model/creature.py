08cd795871fa9bdcc1319f5d0b897ad2374b80e5
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2022-12-06 23:13:58 +0800

    Дополнен файл Creature

--- model/creature.py
+++ model/creature.py
@@ -1,13 +1,23 @@
-# ИСПРАВИТЬ: внимательнее – здесь вы импортировали модуль и именем объекта модуля аннотировали параметры в методах, когда вам нужен объект класса из этого модуля
-import datetime as dt
+from abc import ABC
+
+import self as self
+from utils.constant import KindActions
 
 __all__ = [
-    'Creature'
+    'Creature',
+    'Body',
+    'Mind',
 ]
+
+import patterns as patterns
 
 
 class Body:
-    def __init__(self, health: int,
+    """
+    """
+
+    def __init__(self,
+                 health: int,
                  stamina: int,
                  hunger: int,
                  thirst: int):
@@ -16,14 +26,17 @@ class Body:
         self.hunger = hunger
         self.thirst = thirst
 
+    def tick_changes(self, parameters: 'KindParameters') -> dict:
+        """Собирает изменения за тик"""
+        return {'health': 2, 'stamina': -3, 'hunger': 4, 'thirst': 2}
 
 class Mind:
+
+    patterns = patterns
+
     def __init__(self,
-                 patterns: dict,
                  joy: int,
                  anger: int):
-        # ИСПРАВИТЬ: это должно быть атрибутом класса, не экземпляра
-        self.patterns = patterns
         self.joy = joy
         self.anger = anger
 
@@ -32,19 +45,47 @@ class Mind:
         pass
 
 
-class Creature:
-    def __init__(self, name: str,
-                 birthdate: dt,
+class Creature(ABC):
+    """
+
+    """
+    def __init__(self,
+                 kind_parameters: 'Kindparameters',
                  body: Body,
                  mind: Mind):
-        self.name = name
-        self.birthdate = birthdate
+        self.__kind = kind_parameters
         self.body = body
         self.mind = mind
 
+    @property
+    def age(self):
+        pass
+
+    def apply_tick_changes(self):
+        """"""
+        for state in (self.body, self.mind):
+            for attr, delta in state.tick_changes(self.__kind).items():
+                new_value = getattr(self.body, attr) + delta
+                setattr(self.body, attr, new_value)
+
+
         # tired()
         # sleep()
+    def mainloop(self):
+        """"""
 
+class CreatureActions(Creature):
+    """
+
+    """
+    def run_at_night(self):
+        """"""
+
+    def seek_for_honey(self):
+        """"""
+
+    def get_warm_on_sun(self):
+        """"""
 
 
 class Cat(Creature):
